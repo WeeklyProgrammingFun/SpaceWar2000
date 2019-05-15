b918bad1d45b08237940a4e06ffbeb977114af3b
Brendon McCool <brendonmccool@gmail.com>
2019-05-15 11:39:48 -0400

    Add distance function

--- Pybot/Pybot.py
+++ Pybot/Pybot.py
@@ -2,19 +2,21 @@ import sys
 import logging
 
 logger = logging.getLogger(__name__)
-handler = logging.FileHandler("Pybot.log", mode='w', encoding=None, delay=False)
+handler = logging.StreamHandler()
+#handler = logging.FileHandler("Pybot.log", mode='w', encoding=None, delay=False)
 formatter = logging.Formatter('[%(asctime)s] %(name)-12s %(levelname)-8s --> %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
 class Planet:
-    def __init__(self, x, y, owner, ships, growth):
+    def __init__(self, x, y, owner, ships, growth, index):
         self.x = x
         self.y = y
         self.owner = owner
         self.ships = ships
         self.growth = growth
+        self.index = index
         logger.debug("PLANET CREATED: x:{} y:{} owner:{} ships:{} growth:{}".format(x, y, owner, ships, growth))
 
     def log(self):
@@ -43,11 +45,31 @@ class State:
 
     def create_planet(self, x, y, owner, ships, growth):
         logger.debug("CREATING PLANET: {}".format(len(self.planets)))
-        self.planets.append(Planet(x, y, owner, ships, growth))
+        self.planets.append(Planet(x, y, owner, ships, growth, len(self.planets)))
 
     def create_fleet(self, owner, ships, source, dest, turnsRemaining):
         logger.debug("CREATING FLEET: {}".format(len(self.fleets)))
         self.fleets.append(Fleet(owner, ships, source, dest, turnsRemaining))
+
+    def get_distance(self, pair1, pair2):
+        """
+        Get the distance between two points
+        """
+        x = pair1[0] - pair2[0]
+        y = pair1[1] - pair2[1]
+        return(math.sqrt((x * x) + (y * y)))
+
+    def get_closest_target(self, pair):
+        logger.debug("GETTING CLOSEST TARGET")
+        target = None
+        distance = None
+        for index, planet in enumerate(self.planets):
+            #TODO magic number
+            if planet.owner != str(1):
+                if target == None or self.get_distance(pair, [planet.x, planet.y]) > distance:
+                    target = index
+                    distance = self.get_distance(pair, [planet.x, planet.y])
+        return index
 
     def get_first_enemy_planet(self):
         #TODO fix logger format to put function in log
@@ -81,22 +103,6 @@ class State:
         return planets
 
 class Pybot:
-
-    def make_moves(self, state):
-        moves = "MOVE "
-        friendly_planets = state.get_all_friendly_planets()
-        target = state.get_first_neutral_planet()
-        if target is False: #TODO have to use "is" because 0 == False.  Is there a better way to do this?
-            target = state.get_first_enemy_planet()
-        for planet in friendly_planets:
-            #TODO a planet should know its own index.  That would make this easier.
-            moves = moves + "L {} {} {} ".format(str(planet[0]), str(target), str(planet[1]))
-        moves = moves + "E"
-        logger.debug(moves)
-        moves = moves + "\n"
-        print(moves)
-        sys.stdout.flush()
-
     #TODO can this be taken out???
     def parse_state(self, state):
         logger.debug("DATA INCOMING")
@@ -155,7 +161,7 @@ class Pybot:
             logger.debug("SEED: {}".format(data_in.split()[2]))
         if data_in.split()[0] == "STATE":
             state = self.state_loop(data_in.split("STATE")[1].split())
-            self.make_moves(state)
+            self.make_moves(self, state)
         if data_in.split()[0] == "QUIT":
             return False
         return True
--- Pybot/mybot.py
+++ Pybot/mybot.py
@@ -1,4 +1,30 @@
 from Pybot import Pybot
+import logging
+import sys
 
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+#handler = logging.FileHandler("Pybot.log", mode='w', encoding=None, delay=False)
+formatter = logging.Formatter('[%(asctime)s] %(name)-12s %(levelname)-8s --> %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 mybot = Pybot()
+
+def make_moves(self, state):
+    moves = "MOVE "
+    friendly_planets = state.get_all_friendly_planets()
+    #TODO get geometrical mean
+    target = state.get_closest_target()
+    for planet in friendly_planets:
+        #TODO a planet should know its own index.  That would make this easier.
+        moves = moves + "L {} {} {} ".format(str(planet[0]), str(target), str(planet[1]))
+    moves = moves + "E"
+    logger.debug(moves)
+    moves = moves + "\n"
+    print(moves)
+    sys.stdout.flush()
+
+mybot.make_moves = make_moves
+
 mybot.run()
